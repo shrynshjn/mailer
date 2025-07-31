57462f3c712638017a913fb96cbc5cf2dd8c1965
Shreyansh Jain <shreyanshmacro@gmail.com>
2025-08-01 01:53:05 +0530

    added route for campaing

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const emailRouter = require('./routes/emails');
+const campaignRouter = require('./routes/campaigns');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/emails', emailRouter);
+app.use('/campaigns', campaignRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
